2033fd5b5a2057d38de0200a61aacd12ebbc059c
Lorenzo Spinelli <lorenzo.spinelli@register.it>
2019-09-26 15:40:25 +0200

    fix hook async

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 function App() {
   const [data, setData] = useState({ hits: [] });
-  useEffect(async () => {
-    const result = await axios(
-      'https://hn.algolia.com/api/v1/search?query=redux',
-    );
-    setData(result.data);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://hn.algolia.com/api/v1/search?query=redux',
+      );
+      setData(result.data);
+    };
+    fetchData();
   }, []);
   return (
     <ul>
