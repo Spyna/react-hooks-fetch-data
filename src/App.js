958cf48c1ba62c4985eef16da5cda10d88808b63
Lorenzo Spinelli <lorenzo.spinelli@register.it>
2019-09-26 15:38:04 +0200

    useEffect data fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
   const [data, setData] = useState({ hits: [] });
+  useEffect(async () => {
+    const result = await axios(
+      'https://hn.algolia.com/api/v1/search?query=redux',
+    );
+    setData(result.data);
+  });
   return (
     <ul>
       {data.hits.map(item => (
