ecffafd92c1759d1fc2e2869ea08b4cdb54754eb
Lorenzo Spinelli <lorenzo.spinelli@register.it>
2019-09-26 16:38:23 +0200

    useReducer - finish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment, useState, useEffect,useReducer } from "react";
+import React, { Fragment, useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
 const dataFetchReducer = (state, action) => {
   switch (action.type) {
-    case 'FETCH_INIT':
-      return { ...state };
-    case 'FETCH_SUCCESS':
-      return { ...state };
-    case 'FETCH_FAILURE':
-      return { ...state };
+    case "FETCH_INIT":
+      return { ...state, isLoading: true, isError: false };
+    case "FETCH_SUCCESS":
+      return { ...state, isLoading: false, isError: false, data: action.payload };
+    case "FETCH_FAILURE":
+      return { ...state, isLoading: false, isError: true };
     default:
       throw new Error();
   }
@@ -19,22 +19,22 @@ function useHackerNewsApi(initialSearch, initialData) {
   const [state, dispatch] = useReducer(dataFetchReducer, {
     isLoading: false,
     isError: false,
-    data: initialData,
+    data: initialData
   });
 
   useEffect(() => {
     let didCancel = false;
 
     const fetchData = async () => {
-      dispatch({ type: 'FETCH_INIT' });
+      dispatch({ type: "FETCH_INIT" });
       try {
         const result = await axios(`http://hn.algolia.com/api/v1/search?query=${search}`);
         if (!didCancel) {
-          dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
         }
       } catch (error) {
         if (!didCancel) {
-          dispatch({ type: 'FETCH_FAILURE' });
+          dispatch({ type: "FETCH_FAILURE" });
         }
       }
     };
