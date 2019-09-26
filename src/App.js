0f8f36ee7df71ac523e2d78edc2a33c3cba26909
Lorenzo Spinelli <lorenzo.spinelli@register.it>
2019-09-26 15:41:23 +0200

    trigger hook - start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 function App() {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("redux");
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        'https://hn.algolia.com/api/v1/search?query=redux',
-      );
+      const result = await axios("https://hn.algolia.com/api/v1/search?query=redux");
       setData(result.data);
     };
     fetchData();
   }, []);
+
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 export default App;
